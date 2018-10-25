f03d5f5ddba96aa0c84894f5164753ea6c3b1a34
amm0924 <anurag.mishra@springernature.com>
2018-10-25 15:32:28 +0530

    added constructor

--- account-common-model/src/main/java/com/springernature/oasis/model/Branch.java
+++ account-common-model/src/main/java/com/springernature/oasis/model/Branch.java
@@ -8,6 +8,15 @@ public class Branch {
     private String name;
     private Address address;
 
+    public Branch() {
+    }
+
+    public Branch(String code, String name, Address address) {
+        this.code = code;
+        this.name = name;
+        this.address = address;
+    }
+
     public String getCode() {
         return code;
     }
